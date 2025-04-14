4030d197e16d1d863b928743e0d677c096997c6d
Dinis-17 <dinismedeiros1706@gmail.com>
2025-04-14 14:48:03 +0200

    h

--- Dinis/millores_ui/users_list/usersList.js
+++ Dinis/millores_ui/users_list/usersList.js
@@ -1,2 +1,27 @@
 // Crea una funcio que retorna una llista de usuaris
-function getUsers() {}
+function getUsers() {
+}   
+
+const userList = document.getElementById('userList');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        users.array.forEach(user => {
+            const userCard = document.createElement('div');
+            userCard.classList.add('user-card');
+
+            userCard.innerHTML = `
+                <h3>${user.name}</h3>
+                <p>Email: ${user.email}</p>
+                <button onClick="toggleDetails(${user.id})">Veure més</button>
+                <div id="details-${user.id}" class="hidden">
+                    <p>Telèfon: ${user.phone}</p>
+                    <p>Adreça: ${user.address.street}, ${user.address.city}</p>
+                </div>
+            `;
+
+            userList.appendChild(userCard);
+        });
+    })
+    .catch(error => console.error('Error cargant els usuaris:', error));
