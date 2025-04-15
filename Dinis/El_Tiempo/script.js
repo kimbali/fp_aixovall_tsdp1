dbf342374d15801b9a21950caf54e25fd6352a7a
Manu88ses <malvesm@educand.ad>
2025-04-15 11:29:05 +0200

    temps js

---
+++ Dinis/El_Tiempo/script.js
@@ -0,0 +1,8 @@
+const API_KEY= "43012035ea348394ef9196172a7355c3";
+const url= "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ca&appid=${API_KEY}";
+
+fetch(url)
+.then(res => res.json())
+.then(data =>{
+    console.log(data);
+});
