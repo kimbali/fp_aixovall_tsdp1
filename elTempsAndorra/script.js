c8157aa23e52f162fac947b24fc3d71bcb9ee24c
Dinis-17 <dinismedeiros1706@gmail.com>
2025-05-09 12:11:23 +0200

    hello

--- elTempsAndorra/script.js
+++ elTempsAndorra/script.js
@@ -2,7 +2,6 @@ const API_KEY = '212ebafd80e67458b4fdf2fe666f697f';
 
 const ANDORRA_LATITUDE = 42.5078;
 const ANDORRA_LONGITUDE = 1.5211;
-
 const CATALA = 'ca';
 
 function fetchCurrentWeather() {
@@ -18,7 +17,6 @@ function fetchCurrentWeather() {
       console.log('Vent:', data.wind.speed + ' m/s');
       console.log('Direcció del vent:', data.wind.deg + ' º');
 
-      // Icono + descripcio
       const descripcio = data.weather[0].description;
       const icona = data.weather[0].icon;
 
@@ -27,19 +25,10 @@ function fetchCurrentWeather() {
         'icona'
       ).src = `https://openweathermap.org/img/wn/${icona}@2x.png`;
       document.getElementById('icona').alt = descripcio;
-
-      // Temperatura + temperatura maxima i minima
-
-      // Vent + direccio
-
-      // El día d'avui: Miercoles 16 de Abril
     });
 }
 fetchCurrentWeather();
 
-// Aquest endpoint retorna 40 valors: 8 per dia (cada 3h).
-// Nosaltres agafem només les dades de les 12:00h perquè són les més representatives del migdia.
-// Es pot canviar l'hora buscant per dt_txt.includes("15:00:00"), per exemple.
 function fetchForecast() {
   const urlSetmana = `https://api.openweathermap.org/data/2.5/forecast?lat=${ANDORRA_LATITUDE}&lon=${ANDORRA_LONGITUDE}&units=metric&lang=${CATALA}&appid=${API_KEY}`;
 
@@ -48,7 +37,6 @@ function fetchForecast() {
     .then(data => {
       const diesContainer = document.getElementById('dies');
 
-      // Agrupem per dia (filtrant les 12:00h)
       const previsions = data.list.filter(item =>
         item.dt_txt.includes('12:00:00')
       );
